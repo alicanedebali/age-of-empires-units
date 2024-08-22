64d263df511d7a428f70e1aa4fe0e2347ebdb30b
Alican Edebali <alicanedebali62@gmail.com>
2024-08-22 19:06:03 +0300

    feat(Home): Add age-of-empires image to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import { PageTitle } from '../components';
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="gap-y-4">
       <PageTitle title={'home page'} />
+
+      <img src="/age-of-empires.jpg" alt="Age Of Empires" />
     </div>
   );
 };
